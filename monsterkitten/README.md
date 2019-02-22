83636a16ff8f95e03e0b691583c7aa0c1ba64f70
Ruby Allison Rose <jtimmerman32@gmail.com>
2019-02-22 09:12:42 -0800

    Cleaned up monsterkitten a bit.

--- monsterkitten/README.md
+++ monsterkitten/README.md
@@ -5,14 +5,20 @@ All it does is add download buttons to all the songs on the https://monstercat.c
 I hope it's to your liking.
 
 I wasn't actually planning to release this to the public until it was complete.
-But since I considered that I usually don't finish much I start, metadata assurance was making this a little
-too annoying to work on; and the fact my friend was pushing me, I've finally decided to publish
+But since I considered that I usually don't finish much I start, metadata assurance was making this a little too annoying to work on; and the fact my friend was pushing me, I've finally decided to publish
 this little kitty to the open web. I hope you play nice now.
+
+Kitten's still got a little growing up to do, here are their next milestones:
+ * Button to download zipped albums
+ * Button to download zipped pages
+
+They just went in for a grooming here not to recently so if you wish to inspect
+the source you'll find the contents a bit more readable.
 
 ***And if any devs from monstercat find out I'm sure they'll steal their kitty back away from us.***
 So please, don't ruin the free kitty petting parade.
 
-**Don't forget to support the owners if you have the change!**
+**Don't forget to support the artists if you have the change!**
 
 *Sincerely with Love,*
 **M3TIOR**
--- monsterkitten/monsterkitten.js
+++ monsterkitten/monsterkitten.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         MonsterKitty - Scratching Post
 // @namespace    https://m3tior.github.io/
-// @version      0.3
+// @version      1.0
 // @description  Adds download buttons and funcitonality to monstercat.com/browse
 // @author       M3TIOR
 // @match        https://www.monstercat.com/browse*
 // @grant        GM_xmlhttpRequest
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.min.js
+// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/dist/FileSaver.min.js
 // @require      https://raw.githubusercontent.com/Stuk/jszip/master/dist/jszip.min.js
 /*// require      https://raw.githubusercontent.com/aadsm/jsmediatags/master/dist/jsmediatags.min.js*/
 // ==/UserScript==
@@ -33,84 +33,82 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
+
+// Save a global reference so we don't drop it over and over again and
+// kill our performance like mad *facepalm*
+var observer = null;
+
+function ripSong(event){ // rips button contents
+	var btn = event.target.parentNode;
+	var ddl = btn.getAttribute("download-link");
+	var file_name = btn.getAttribute("artists-title") + " - " + btn.getAttribute("title");
+	// WHAT A PAIN THIS THING IS... SO JANK... thx https://github.com/greasemonkey/greasmonkey/issues/1834
+	GM_xmlhttpRequest({
+		method: "GET",
+		url: ddl,//"https://s3.amazonaws.com/data.monstercat.com/blobs/"+ddl.split("/")[4],
+		overrideMimeType: "text/plain; charset=x-user-defined",
+		onload: function(xhr){
+			var r = xhr.responseText;
+			var data = new Uint8Array(r.length);
+			for (var i = 0; i < r.length; i++) data [i] = r.charCodeAt(i);
+			var blob = new Blob([data], {type:"audio/mpeg"});
+			saveAs(blob, file_name+".mp3");
+		}
+	});
+}
+
+function ripAlbum(element){ // rips album contents
+	// TODO
+}
+
+function ripPage(element){ // rips current page contents
+	// TODO
+}
+
+function injectButtons(){
+	document.querySelectorAll("[dblc-action='playSongDblC']").forEach(function(song){
+		if (song.querySelector("[role='download-song']")) return;
+		if (!song.querySelector("[role='play-song']")) return; // skip special releases
+
+		// store the play button we're going to grab data from
+		var playbtn = song.querySelector("[role='play-song']").parentNode;
+
+		// copy the info of our play button table element into a new one
+		var buttonC = playbtn.cloneNode(true);
+		// focus on the button and clear it's contents but preserve attributes
+		var button = buttonC.children[0];
+
+		button.innerHTML="<i class='fa fa-download'></i>"; // font-awesome download button.
+		button.setAttribute("role", "download-song");	// change the role so we can find processed elements later.
+		button.setAttribute("style", "padding-left: 5px;"); // add some padding to improve usability
+
+		// switch the play-link attribute to download-link
+		button.setAttribute("download-link", button.getAttribute("play-link"));
+
+		// remove old unused attributes
+		button.removeAttribute("play-link");
+		button.removeAttribute("action");
+
+		// set the onclick for the button so it'll trigger
+		//button.onclick = Srip;
+		button.addEventListener("click", ripSong);
+
+		song.insertBefore(buttonC, playbtn.nextElementSibling);
+	});
+};
+
 (function() {
 	'use strict';
 
-	var Srip = function(event){ // rips button contents
-		var btn = event.target.parentNode;
-		var ddl = btn.getAttribute("download-link");
-		var file_name = btn.getAttribute("artists-title") + " - " + btn.getAttribute("title");
-		// WHAT A PAIN THIS THING IS... SO JANK... thx https://github.com/greasemonkey/greasmonkey/issues/1834
-		GM_xmlhttpRequest({
-			method: "GET",
-			url: ddl,//"https://s3.amazonaws.com/data.monstercat.com/blobs/"+ddl.split("/")[4],
-			overrideMimeType: "text/plain; charset=x-user-defined",
-			onload: function(xhr){
-				var r = xhr.responseText;
-				var data = new Uint8Array(r.length);
-				for (var i = 0; i < r.length; i++) data [i] = r.charCodeAt(i);
-				var blob = new Blob([data], {type:"audio/mpeg"});
-				saveAs(blob, file_name+".mp3");
-			}
+	waitForKeyElements("button[role='play-song']",()=>{
+		injectButtons();
+
+		observer = new MutationObserver((mutation, observer)=>{
+			waitForKeyElements("button[role='play-song']",injectButtons());
 		});
-	};
-	var Arip = function(element){ // rips album contents
-		// TODO
-	};
-	var Prip = function(element){ // rips current page contents
-		// TODO
-	};
 
-	var observe = function(target){
-		var observer = null;
-		var promise = new Promise(resolve => {
-			observer = new MutationObserver(function(mutations) {
-				resolve(true);
-			});
-		}).then(function(value){
-			observer.disconnect();
-		});
-		observer.observe(
-			target, {childList: true} // only need childlist in this case
-		);
-		return promise;
-	};
+		let browseTab = document.querySelector("[role='browse-pages']");
+		observer.observe(browseTab, {childList: true});
+	});
 
-	var UpdateDOM = function(){
-		document.querySelectorAll("[dblc-action='playSongDblC']").forEach(function(song){		// for each song on the screen
-			if (song.querySelector("[role='download-song']")) return;							// if we haven't already made a play button for it.
-			if (!song.querySelector("[role='play-song']")) return;                              // skip special releases
-			var playbtn = song.querySelector("[role='play-song']").parentNode;					// store the play button we're going to grab data from
-			var buttonC = playbtn.cloneNode(true);												// copy the info of our play button table element into a new one
-			var button = buttonC.children[0]; 													// focus on the button
-																								// clear it's contents but preserve attributes
-			//button.innerHTML="<svg><use xlink:href=\"#download-btn-svg\"></use></svg>"		// and add the download button in.
-
-			/* XXX:
-			 *		ok, so actually, monstercat.com as of now uses fontawesome.io so I didn't have to make a download button...
-			 */
-			button.innerHTML="<i class='fa fa-download'></i>";									// start by creating the download button.
-			button.removeAttribute("action");													// remove the action trigger for monstercat's javascript
-			button.setAttribute("role", "download-song");										// change the role so we can find processed elements later.
-			button.setAttribute("style", "padding-left: 5px;");                                 // add some padding to improve usability
-			button.setAttribute("download-link", button.getAttribute("play-link"));				// switch the play-link attribute to download-link
-			button.removeAttribute("play-link");												// cleanup for download-link \ rmove old play-link attribute
-			button.onclick = Srip;																// set the onclick for the button so it'll trigger
-			//button.addEventListener("click", Srip);
-
-			song.insertBefore(buttonC,playbtn.nextElementSibling);
-		});
-	};
-
-	// Apperently, Tampermonkey's editor doesn't like this async syntax... too bad... it's just fine...
-	var ManageDOM = async function(){
-		// upkeep loop
-		while (true){
-			UpdateDOM();
-			var screen_update = observe(document.querySelector("[role='browse-pages']"));
-			await screen_update;
-		}
-	};
-
-	var one=false; waitForKeyElements("button[role='play-song']",function(e){if (!one){ one=true; ManageDOM(); }}); // await everything necessary and schedule DOM manager
 })();
