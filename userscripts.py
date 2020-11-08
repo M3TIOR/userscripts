e0ffbe1ae4f6dd416dc3eec50019273217aa20d3
Ruby Allison Rose <M3TIOR@users.noreply.github.com>
2020-11-08 00:48:18 -0800

    chore: Removed unnecessary junk file I never finished.

--- userscripts.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# encoding=utf-8
-#
-# Userscripts.py: controls distribution and publishing of this repository
-# Copyright (C) 2018 Ruby Allison Rose
-#
-# Oof I don't remember what I wanted to use that for...
-
-"""DOCUMENT ME!!!"""
