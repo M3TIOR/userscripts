72f8f227888f4460ba951ddb91dc4d0ca005f2ac
Ruby Allison Rose <M3TIOR@users.noreply.github.com>
2020-11-08 01:21:36 -0800

    chore: Optimized script for compiler minification and enforced hard tab standard.

--- nyantube/nyantube.user.js
+++ nyantube/nyantube.user.js
@@ -19,62 +19,61 @@
 const Base64 = {
 	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 	decode : (input) => {
-	    let output = [];
-	    let chr1, chr2, chr3;
-	    let enc1, enc2, enc3, enc4;
-	    let i = 0;
+		let output = [];
+		let chr1, chr2, chr3;
+		let enc1, enc2, enc3, enc4;
+		let i = 0;
 
-			// sanitizer
-	    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+		// sanitizer
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
-	    while (i < input.length) {
-	        enc1 = Base64._keyStr.indexOf(input.charAt(i++));
-	        enc2 = Base64._keyStr.indexOf(input.charAt(i++));
-	        enc3 = Base64._keyStr.indexOf(input.charAt(i++));
-	        enc4 = Base64._keyStr.indexOf(input.charAt(i++));
+		while (i < input.length) {
+			enc1 = Base64._keyStr.indexOf(input.charAt(i++));
+			enc2 = Base64._keyStr.indexOf(input.charAt(i++));
+			enc3 = Base64._keyStr.indexOf(input.charAt(i++));
+			enc4 = Base64._keyStr.indexOf(input.charAt(i++));
 
-	        chr1 = (enc1 << 2) | (enc2 >> 4);
-	        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-	        chr3 = ((enc3 & 3) << 6) | enc4;
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
 
-	        output.push(chr1);
+			output.push(chr1);
 
-	        if (enc3 != 64) output.push(chr2);
-	        if (enc4 != 64) output.push(chr3);
-	    }
+			if (enc3 != 64) output.push(chr2);
+			if (enc4 != 64) output.push(chr3);
+		}
 
-	    return output;
+		return output;
 	},
 };
 
 (function() {
-  'use-strict';
+	'use-strict';
 
 	// Base 64 encoded Nyan Cat GIF
-	const gif64 = [
-		"R0lGODlhIQAVAMIGAAAAAP8zmZmZmf+Z///Mmf///6KakaKakSH/C05FVFNDQVBFMi4wAw",
-		"EAAAAh/hFDcmVhdGVkIHdpdGggR0lNUAAh+QQJAQAHACwAAAAAIQAVAAADnHi6C/4wOkYr",
-		"IDjrjUH9FzaMZElm3seE5hCMb9mpKwG3+ECk9HHlI8dN55mofjmAQACQOZYpiAJZiimhpy",
-		"tzCi3aksvuMxy9Pr4ukzbMhjbM1Oq63eaNf7GStjDnu98OaGpkWkp3FnFpA0ojdGFBICJ5",
-		"QQKNZlCVi5EiLYUSfxRnHCiOZCCEEqkSR10+oFxbZjRSPkahRrY9ursKCQAh+QQJAQAHAC",
-		"wAAAAAIQAVAAADnXi6C/4wOkYrIDjrjUH9FzaMZElm3seE5hCMb9mpKwG3+ECk9HHlJIdr",
-		"tFNMQDYgQCAAnDzLJrIVG0SbRAdT6uP9lNvmIwxlpr5D0zXMLosdSdNrzWYbybZq6Vqg95",
-		"kNUWgtV1pmYoArcHJBAiN1WyNIeYSOA02HmJIWGTmFEmYgIRwokFs8K2YSq6wqgj6hFK+v",
-		"rkddqIFntj28vQkAIfkECQEABwAsAAAAACEAFQAAA514utz+0IFJq51Rks2730C2ANxgnu",
-		"jJhSJJpENgyigoHuQM7wPBZjmeaRIz+RSYR5AHEAgAqlDzqXylaIPp0zhxUjXCrPdJGf8Y",
-		"S+xJO26fkUuUjN1ujyov9dpboPedOGxxKVpdTk2GOF4UVidYTSZ1XiZwGC5FhAKRU5yaA1",
-		"UbTIsXh0ouHyuSixCIF66vEZyKX2ilsqxJgW+VvAcJACH5BAkBAAcALAAAAAAhABUAAAOf",
-		"eLrc/tCBSaudUZLNu99AtgDcYJ7oyYUiSaRDYMooKB7kDO8DwWY5nmkSM/kUmEeQBxAIAK",
-		"pQ86l8pWiD6dM4cVI1wqz3SRn/GEvsSTtun3ET61XcrjtH05x67S2wBX53OGZyMFpdTk2I",
-		"g2Rxc0MCJnZOJowsLkUpTZJ5iZEDSD8uhSiHF4lKox8gk6gSiRexshB5lq9UtbRJcG+hl7",
-		"sJACH5BAkBAAcALAAAAAAhABUAAAOeeLrc/tCBSaudUZLNu99AtgDcYJ7oyYUiSaRDYMoo",
-		"KB7kDO8DwWY53mCi64UwkOAOIBAAapPmr6E80ZhSFdapqHRfvK2T0pTiyhVwLCUuu7lRdN",
-		"XafrtZFeeLhtoW2n9mDBRqbHJoY4JUhWtDAiZ2ZSZTIxuNJ0yQWJuPQxIujJNyFoojhB8r",
-		"kWgSoxeuFw+bZ1xUZrJJSDi5g0gUDQkAIfkECQEABwAsAAAAACEAFQAAA514utz+0IFJq5",
-		"1Rks2730C2ANxgnujJhSJJpENgyigoHuQMD1NKhBhIbscTCAAx0w9gZGmIxaaK2Rw5hyga",
-		"tUkxVnFfLGzrLYe5k5fuRDabWWTxKbAttO1fHDVHQ20nXkyARw0UamMCJm5eJk4MclqJA0",
-		"dNlY0PLkkpfxd5jy6Hm4uBmEegHyAXFKVoqq6EQlV7ErKehUGrEkF6jgcJADs="
-	].join("");
+	const gif64 =
+		"R0lGODlhIQAVAMIGAAAAAP8zmZmZmf+Z///Mmf///6KakaKakSH/C05FVFNDQVBFMi4wAw" +
+		"EAAAAh/hFDcmVhdGVkIHdpdGggR0lNUAAh+QQJAQAHACwAAAAAIQAVAAADnHi6C/4wOkYr" +
+		"IDjrjUH9FzaMZElm3seE5hCMb9mpKwG3+ECk9HHlI8dN55mofjmAQACQOZYpiAJZiimhpy" +
+		"tzCi3aksvuMxy9Pr4ukzbMhjbM1Oq63eaNf7GStjDnu98OaGpkWkp3FnFpA0ojdGFBICJ5" +
+		"QQKNZlCVi5EiLYUSfxRnHCiOZCCEEqkSR10+oFxbZjRSPkahRrY9ursKCQAh+QQJAQAHAC" +
+		"wAAAAAIQAVAAADnXi6C/4wOkYrIDjrjUH9FzaMZElm3seE5hCMb9mpKwG3+ECk9HHlJIdr" +
+		"tFNMQDYgQCAAnDzLJrIVG0SbRAdT6uP9lNvmIwxlpr5D0zXMLosdSdNrzWYbybZq6Vqg95" +
+		"kNUWgtV1pmYoArcHJBAiN1WyNIeYSOA02HmJIWGTmFEmYgIRwokFs8K2YSq6wqgj6hFK+v" +
+		"rkddqIFntj28vQkAIfkECQEABwAsAAAAACEAFQAAA514utz+0IFJq51Rks2730C2ANxgnu" +
+		"jJhSJJpENgyigoHuQM7wPBZjmeaRIz+RSYR5AHEAgAqlDzqXylaIPp0zhxUjXCrPdJGf8Y" +
+		"S+xJO26fkUuUjN1ujyov9dpboPedOGxxKVpdTk2GOF4UVidYTSZ1XiZwGC5FhAKRU5yaA1" +
+		"UbTIsXh0ouHyuSixCIF66vEZyKX2ilsqxJgW+VvAcJACH5BAkBAAcALAAAAAAhABUAAAOf" +
+		"eLrc/tCBSaudUZLNu99AtgDcYJ7oyYUiSaRDYMooKB7kDO8DwWY5nmkSM/kUmEeQBxAIAK" +
+		"pQ86l8pWiD6dM4cVI1wqz3SRn/GEvsSTtun3ET61XcrjtH05x67S2wBX53OGZyMFpdTk2I" +
+		"g2Rxc0MCJnZOJowsLkUpTZJ5iZEDSD8uhSiHF4lKox8gk6gSiRexshB5lq9UtbRJcG+hl7" +
+		"sJACH5BAkBAAcALAAAAAAhABUAAAOeeLrc/tCBSaudUZLNu99AtgDcYJ7oyYUiSaRDYMoo" +
+		"KB7kDO8DwWY53mCi64UwkOAOIBAAapPmr6E80ZhSFdapqHRfvK2T0pTiyhVwLCUuu7lRdN" +
+		"XafrtZFeeLhtoW2n9mDBRqbHJoY4JUhWtDAiZ2ZSZTIxuNJ0yQWJuPQxIujJNyFoojhB8r" +
+		"kWgSoxeuFw+bZ1xUZrJJSDi5g0gUDQkAIfkECQEABwAsAAAAACEAFQAAA514utz+0IFJq5" +
+		"1Rks2730C2ANxgnujJhSJJpENgyigoHuQMD1NKhBhIbscTCAAx0w9gZGmIxaaK2Rw5hyga" +
+		"tUkxVnFfLGzrLYe5k5fuRDabWWTxKbAttO1fHDVHQ20nXkyARw0UamMCJm5eJk4MclqJA0" +
+		"dNlY0PLkkpfxd5jy6Hm4uBmEegHyAXFKVoqq6EQlV7ErKehUGrEkF6jgcJADs=";
 
 	// Transforms the gif Base64 string into a byte array in memory
 	const gifBytes = new Uint8Array(Base64.decode(gif64));
@@ -85,37 +84,34 @@ const Base64 = {
 
 
 	// This is the CSS for controling youtube's bar display.
-	const css = (`
-		.ytp-play-progress {
-			padding: 2.5px 1px;
-			top: -2px;
-			background: linear-gradient(
-				to bottom,
-				#f00 0%, #f90 17%, #ff0 33%,
-				#3f0 50%, #09f 67%, #63f 83%
-			);
-		}
-		.ytp-scrubber-container {
-			background: url(${gifURL}) no-repeat;
-			background-size: 36px;
-			width: 40px;
-			height: 24px;
-			margin-top: -4px;
-		}
-		.ytp-scrubber-container:hover {
-			background-size: 40px;
-			margin-top: -6px;
-			margin-left: -2px;
-		}
-		.ytp-scrubber-button {
-			display: none;
-		}
-	`)
-		// Removes all tabs
-		.replace(/\t/g, "")
-		// And replaces newlines with spaces because the CSS3 parsers
-		// in certain browsers may need whitespace after properties.
-		.replace(/\n/g, " ");
+	// I tried to make it look nice, It's optimized for the compiler now.
+	// Still is going to be ass to maintain.
+	const css = (
+		".ytp-play-progress { "+
+			"padding: 2.5px 1px; "+
+			"top: -2px; "+
+			"background: linear-gradient( "+
+				"to bottom, "+
+				"#f00 0%, #f90 17%, #ff0 33%, "+
+				"#3f0 50%, #09f 67%, #63f 83%; "+
+			"); "+
+		"} "+
+		".ytp-scrubber-container { "+
+			"background: url(${gifURL}) no-repeat; "+
+			"background-size: 36px; "+
+			"width: 40px; "+
+			"height: 24px; "+
+			"margin-top: -4px; "+
+		"} "+
+		".ytp-scrubber-container:hover { "+
+			"background-size: 40px; "+
+			"margin-top: -6px; "+
+			"margin-left: -2px; "+
+		"} "+
+		".ytp-scrubber-button { "+
+			"display: none; "+
+		"}"
+	);
 
 	// The typeof requests are necessary to protect strict evaluation from
 	// terminating us early when the tested values are undefined *facepalm*
